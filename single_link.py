31a91bc60e683bdc03254bae6625ada70fc48113
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-02-16 20:58:01 +0530

    Add files via upload

---
+++ single_link.py
@@ -0,0 +1,21 @@
+ #traversal only in one direction
+ #traveersal means go with the rule 1-2-3-4-5
+
+#node in single list list
+class Node:
+    def __init__(self,data):
+        self.data =data
+        self.reference = None
+node1 = Node(7)
+print(node1.data)
+print(node1.reference)
+
+# clas of single linked list
+class linked:
+    def __init__(self):
+        self.head = None
+        print("Gaurav")
+s = linked()
+
+ 
+
