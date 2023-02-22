38428421d0c977399e44a46916fe10449c789fa1
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-02-22 22:37:47 +0530

    Add files via upload

--- search_BST.py
+++ search_BST.py
@@ -4,16 +4,20 @@ class Node:
         self.right = None
         self.left = None
     
-    def insert(self,data):
-        if self is None:
-            return Node(data)
-        if data < self.data:
-            self.left = self.left.insert(data)
+    def search(self,val):
+        if self.data > val:
+            if self.left is None:
+                return "node not present:",self.data
+            return self.left.search(val)
+        elif self.data <val:
+            if self.right is None:
+                return "node is not present:",self.data
+            return self.right.search(val)
         else:
-            self.right = self.right.insert(data)
-            
-        return 
-    
+            return "key i found in tree" ,self.data
+         
+                    
+     
 def inorder(root):
     if root is not None:
         inorder(root.left)
@@ -22,39 +26,45 @@ def inorder(root):
         
         inorder(root.right)
 
-    
+def insert(node,data):
+    if node is None:
+        return Node(data)
+    if data < node.data:
+        node.left = insert(node.left,data)
+    else:
+        node.right = insert(node.right, data)
+        
+    return node
 
 #def search(root,key):
-#        if root.data ==key:
-#           print("not is found")
-#           return 
-#        if root.data < key:
-#            if root.left:
-#                root.left.search(key)
-#                return
-#            else:
-#                print("node is not found")
-#        else:
-#            if root.data > key:
-#                if root.right:
-#                    root.right.search(key)
-#                    return
-#                else:
-#                    print("node not found")
-                       
-    
-    
-    
-    
-    
-    
-root = Node(1)
-root = Node(23)
-root = Node(20)
-root = Node(5)
-root = Node(8)
-root = Node(4)
-root = Node(3) 
-inorder(root)
+#    if root.data is None:
+#        return None
+#    if root.data == key:
+#        return root.data
+#    if root.data > key:
+#        root.left = search(root.left,key)
+#    else:
+#       root.right =  search(root.left,key)
 
-#search(root,3)
+
+            
+            
+            
+            
+    
+   
+root = Node(7)
+root = insert(root,23)
+root = insert(root,20)
+root = insert(root,5)
+root = insert(root,8)
+root = insert(root,4)
+root = insert(root,3) 
+inorder(root)      
+print()  
+
+
+print(root.search(5))
+print(root.search(9))                
+
+
