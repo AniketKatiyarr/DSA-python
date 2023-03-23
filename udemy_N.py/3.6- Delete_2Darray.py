2c0e37d319a2b9c06420dd2427f4a5af1606bb68
Aniket Katiyar <118245999+Aniketkatiyr@users.noreply.github.com>
2023-03-23 23:20:43 +0530

    Add files via upload

---
+++ udemy_N.py/3.6- Delete_2Darray.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+twoarray = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+newArray = np.delete(twoarray, 0 , axis=0)
+print(newArray)
+
+# Time Complexity:-- O(mn)
+#if delete last row or column then complexi 
+
+
+
+
+    
